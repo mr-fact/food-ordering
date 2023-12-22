63e4bcfa38b3fdcc0e27e8cb9db8e0caa1e62bba
mr-fact <mr.fact.202020@gmail.com>
2023-12-23 00:18:57 +0330

    create new end-point to get the price list

--- food_ordering/food/urls.py
+++ food_ordering/food/urls.py
@@ -1,3 +1,10 @@
+from rest_framework.routers import DefaultRouter
+
+from food.views import PriceViewSet
+
+router = DefaultRouter()
+router.register('api/price', PriceViewSet, basename='price')
+
 urlpatterns = [
 
-]
+] + router.urls
