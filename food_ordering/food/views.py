eb749578dc52155a71c1fce4832704583ae2b753
mr-fact <mr.fact.202020@gmail.com>
2023-12-23 00:19:21 +0330

    create new end-point to get the price list (add PriceViewSet)

--- food_ordering/food/views.py
+++ food_ordering/food/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework import mixins
+from rest_framework.viewsets import ViewSet, GenericViewSet
 
-# Create your views here.
+from food.models import Price
+from food.serializers import PriceSerializer
+
+
+class PriceViewSet(
+    mixins.RetrieveModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet
+):
+    serializer_class = PriceSerializer
+
+    def get_queryset(self):
+        if self.action == 'list':
+            return Price.objects.filter(food__hidden=False, status__in=[Price.ACTIVE, Price.INACTIVE, ])
+        else:
+            return Price.objects.filter(food__hidden=False)
