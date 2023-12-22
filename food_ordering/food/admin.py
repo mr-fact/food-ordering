f8def18531247c7b7f38c42e8b46f91a77fc37b2
mr-fact <mr.fact.202020@gmail.com>
2023-12-23 00:19:38 +0330

    add new classes to admin panel

--- food_ordering/food/admin.py
+++ food_ordering/food/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from food.models import Food, Price, Category
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+class PriceInline(admin.TabularInline):
+    model = Price
+    extra = 1
+
+
+@admin.register(Food)
+class FoodAdmin(admin.ModelAdmin):
+    inlines = [PriceInline, ]
+
+
+@admin.register(Price)
+class PriceAdmin(admin.ModelAdmin):
+    pass
